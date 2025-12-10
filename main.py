4829ed79c7848ecae82d057f9fbbf9aaf63b5a2e
SeengaXisse <98480045+SeengaXisse@users.noreply.github.com>
2025-12-09 22:23:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from clini_data.storage import cargar_datos
-from clini_data.ui_pyqt.main_window import MainWindow
-import sys
 from PyQt5.QtWidgets import QApplication
+from clini_data.ui_pyqt import resources
+import pathlib
 
-if __name__ == "__main__":
-    cargar_datos()
-    app = QApplication(sys.argv)
-    ventana = MainWindow()
-    ventana.show()
-    sys.exit(app.exec_())
+app = QApplication(sys.argv)
+
+# Cargar QSS
+qss_path = pathlib.Path(__file__).parent / "clini_data" / "ui_pyqt" / "styles.qss"
+if qss_path.exists():
+    with open(qss_path, "r", encoding="utf-8") as f:
+        app.setStyleSheet(f.read())
